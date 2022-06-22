06f4ee5ac29be9b13067e3b48178ed371dbc4817
Muhammad Arslan <67947459+Muhammad-Arslan-Ahmad@users.noreply.github.com>
2022-06-22 23:01:11 +0500

    sort by unique views and code refactor

--- lib/page_views.rb
+++ lib/page_views.rb
@@ -1,21 +1,28 @@
 # frozen_string_literal: true
 
+require_relative 'decorators/most_view'
+require 'byebug'
+
 class PageViews
   def initialize(page_views)
     @page_views = page_views
   end
 
   def most_views
-    formatted_views
+    Decorators::MostView.new(@page_views).count
+  end
+
+  def unique_views
+    formatted_unique_views
   end
 
   private
 
-  def most_views_sorted
-    @page_views.sort_by { |_page, views| -views.count }
+  def unique_views_sorted
+    @page_views.sort_by { |_page, views| -views.uniq.count }
   end
 
-  def formatted_views
-    most_views_sorted.map { |page, views| "#{page} #{views.count} visits" }
+  def formatted_unique_views
+    unique_views_sorted.map { |page, views| "#{page} #{views.uniq.count} unique views" }
   end
 end
--- parser.rb
+++ parser.rb
@@ -2,6 +2,9 @@
 
 # frozen_string_literal: true
 
-require_relative 'lib/console_printer.rb'
+require_relative 'lib/log_parser.rb'
+require_relative 'lib/page_views.rb'
 
-ConsolePrinter.new('webserver.log').print_counts
+page_views = LogParser.new('webserver.log').parse
+# puts PageViews.new(page_views).most_views
+puts PageViews.new(page_views).unique_views
